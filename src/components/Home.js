7e6ef7967ca75f7ea1f4b208628456d7793e2b5e
Syed Bilal Ur Rehman <39633744+SyedRehman101@users.noreply.github.com>
2023-02-06 23:41:48 +0500

    deploying app

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import selectedMarker from '../assets/img/selectedMarker.svg'
 import Modal from './Modal';
 import Draggable from 'react-draggable';
-
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 const Home = () => {
     const gridNumber = 60000;
 
@@ -39,20 +39,20 @@ const Home = () => {
 
     useEffect(() => {
 
-        window.addEventListener("wheel", (e) => {
-            if (e.deltaY > 0.2) {
-                ZoomOut()
-            }
-            else if (e.deltaY < 2) {
-                ZoomIn()
-            }
-            else { }
-        })
+        // window.addEventListener("wheel", (e) => {
+        //     if (e.deltaY > 0.2) {
+        //         ZoomOut()
+        //     }
+        //     else if (e.deltaY < 2) {
+        //         ZoomIn()
+        //     }
+        //     else { }
+        // })
 
         let boxes = "";
 
         for (let i = 0; i < gridNumber; i++) {
-            boxes += '<button name="btns" class=" focus:bg-red-700 w-2 h-2 border-t-[0.5px] border-l-[0.5px]  border-blue-800 flex-none" id="box-' + i + '"></button>'
+            boxes += '<button name="btns" class=" active:bg-red-700 w-2 h-2 border-t-[0.5px] border-l-[0.5px]  border-blue-800 flex-none" id="box-' + i + '"></button>'
         }
 
         document.getElementById('grid-box').innerHTML = boxes;
@@ -60,53 +60,68 @@ const Home = () => {
         const btns = [...document.getElementsByName("btns")];
 
 
-        btns.forEach((elem) => {
+        btns.forEach((elem, index) => {
             elem.addEventListener('click', (e) => {
-                const img = document.getElementById('marker');
+                // const index = elem.id.split('-');
+                // const img = document.getElementById('img-' + index);
 
+                // img.classList.remove('hidden')
                 setSelectedBoxId(elem.id)
 
                 const modal = document.getElementById("modal");
 
                 modal.classList.remove('invisible');
 
-                img.style.left = `${e.pageX - 14.45}px`;
-                img.style.top = `${e.pageY - 47}px`;
-                img.style.right = `${-e.pageX - 14.45}`
+
+                // img.style.right = `${-e.pageX - 14.45}`
             })
         })
 
     }, [])
 
     return (
-        <Draggable>
-            <div className="w-full gap-4 h-screen overflow-auto">
+        <>
+            <div className="w-full gap-4 h-screen overflow-hidden">
                 <div className="w-full col-span-4 h-full flex justify-center items-center">
-                    <div id="grid-container" className="w-full h-[45rem]  absolute transition-all duration-300 flex" >
-                        <div className="w-full transition-all  duration-300 h-full flex flex-row flex-wrap bg-gray-900 cursor-pointer overflow-hidden" id="grid-box" >
-                        </div>
-                        <div id='marker' className='flex justify-center absolute items-center'>
-                            <img src={selectedMarker} />
-                        </div>
-                        <div className='fixed bottom-5 left-10 z-20'>
+                    <TransformWrapper
+                        initialScale={1}
 
-                            <button onClick={() => {
-                                ZoomOut();
-                            }} className=' bg-white w-10 p-1'>
-                                -
-                            </button>
-                            <button onClick={() => {
-                                ZoomIn()
+                    >
+                        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                            <>
+                                <TransformComponent>
+                                    <Draggable>
+                                        <div id="grid-container" className=" w-full h-[55rem]  relative transition-all duration-300 flex" >
+                                            <div className="w-full transition-all relative  duration-300 h-full flex flex-row flex-wrap bg-gray-900 cursor-pointer overflow-hidden" id="grid-box" >
+                                            </div>
+                                            <div id='marker' className='flex justify-center absolute items-center -translate-y-12 -translate-x-2'>
 
-                            }} className='  bg-white w-10 p-1'>
-                                +
-                            </button>
-                        </div>
-                        <Modal selectedBoxId={selectedBoxId} />
-                    </div>
+                                            </div>
+                                        </div>
+                                    </Draggable>
+
+                                </TransformComponent>
+                                <div className='fixed bottom-5 left-10 z-20'>
+
+                                    <button onClick={() => {
+                                        zoomOut();
+                                    }} className=' bg-white w-10 p-1'>
+                                        -
+                                    </button>
+                                    <button onClick={() => {
+                                        zoomIn()
+
+                                    }} className='  bg-white w-10 p-1'>
+                                        +
+                                    </button>
+                                </div>
+                            </>
+                        )}
+                    </TransformWrapper>
                 </div>
             </div>
-        </Draggable>
+            <Modal selectedBoxId={selectedBoxId} />
+        </>
     )
 }
 
