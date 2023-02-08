34adf3d19a2d4bdd06ea6663a1aa95411d2424e5
SyedRehman101 <syedrehman.solve@gmail.com>
2023-02-08 10:43:50 +0500

    new modal added

--- src/components/BuyModal.jsx
+++ src/components/BuyModal.jsx
@@ -3,21 +3,40 @@ import LandPreview from '../assets/img/LandPreview.avif';
 import { AiOutlineClose } from 'react-icons/ai';
 import { FaFacebookF, FaMapMarkerAlt, FaRedditAlien, FaTwitter } from 'react-icons/fa';
 import { Input } from '@material-tailwind/react';
+import { toast, ToastContainer } from 'react-toastify';
 const BuyModal = (props) => {
+
+    const SubmitHandler = (e) => {
+        e.preventDefault();
+        toast('🦄 Details Secured', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+
+    }
+
+
     return (
+            
         <div id='buy-modal' className='absolute scale-0 p-8 w-1/3 border-0 border-gray-100 top-28 rounded-lg bg-black z-30 h-auto left-1/3 transition-all duration-500'>
-            <div className='flex justify-end'>
+            <div className='flex justify-end transition-all'>
                 <button onClick={() => {
                     const div = document.getElementById("buy-modal");
                     div.classList.add('scale-0')
                 }}>
-                    <AiOutlineClose className='text-gray-400' />
+                    <AiOutlineClose className='text-gray-400  hover:animate-spin hover:text-purple-500' />
                 </button>
             </div>
 
             <br />
-            <form onSubmit={(e) => console.log(e)}>
-
+            <form onSubmit={(e) => SubmitHandler(e)}>
+                
                 <Input variant='standard' color="purple" label="Name" />
 
                 <br />
